944c389455188ca1333c616635931cdb77faa8a1
khanhtran <khanhtrantung190894@gmail.com>
2019-10-07 00:58:59 +0700

    authen redirec to log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/home'
-  get '/app', to: 'welcome#app', as: 'app'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
+
+  root to: 'welcome#app'
+  # get 'welcome/home'
+  # get '/app', to: 'welcome#app', as: 'app'
   namespace :api do 
     namespace :v1 do 
       resources :posts
